df95cbf2543d1dee1ffb799c8eb1b8243a0c2187
ldcf <luluc.fontes@gmail.com>
2021-06-27 23:20:47 +0000

    Negativos resolvida

--- negativos5/src/main.cpp
+++ negativos5/src/main.cpp
@@ -1,12 +1,35 @@
 #include <iostream>
+#include <vector>
+#include <array>
+using namespace std;
 using std::cin;
 using std::cout;
-
+using std::endl;
 const int SIZE = 5; // input size.
 
 int main(void)
 {
-    // TODO: Adicione aqui seu código.
+    vector<int> v;
+    int cont = 0;
+
+    while(true){
+        int x;
+        cin >> std::ws >> x;
+
+        if(cin.eof())
+        break;
+
+        v.push_back(x);
+    }
+
+    for(int i=0; i<v.size(); i++){
+        if(v[i]<0){
+            cont++;
+        }
+    }
+
+    cout << cont << endl;
+    
 
     return 0;
 }
