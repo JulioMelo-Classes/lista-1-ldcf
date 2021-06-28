60ca5ab0dc2db5ec3c1556eaa2e357efe836003f
ldcf <luluc.fontes@gmail.com>
2021-06-28 02:04:18 +0000

    atualizando

--- soma_vizinhos/src/main.cpp
+++ soma_vizinhos/src/main.cpp
@@ -4,13 +4,86 @@
  * @data June, 6th 2021
  */
 #include <iostream>
-using std::cout;
+#include <vector>
+#include <array>
+using namespace std;
 using std::cin;
+using std::cout;
 using std::endl;
 
-int main( void )
+int main(void)
 {
-    // TODO: Adicione seu código aqui. 
+    vector<int> v;
+    int m=0,n=0,range=0,saida=0,temp=0;
+
+    while(true){
+        
+        cin >> std::ws >> m;
+        cin >> std::ws >> n;
+
+        if (cin.eof()){
+            break;
+        }
+        
+        if(n>0){
+            if(m>n){
+                range = m - n;
+                saida = m;
+                temp = saida -1;
+
+                for(int i=0; i<range; i++){
+                    saida = saida + temp;
+                    temp = saida -1;
+                }  
+            }
+        
+            if(m<n){
+                range = m - n;
+                saida = m;
+                temp = saida +1;
+
+                for(int i=0; i<range; i++){
+                    saida = saida + temp;
+                    temp = saida +1;
+                }  
+            }
+            cout << saida << endl;    
+        }
+
+        if(n<0){
+            if(m>n){
+                range = m + n;
+                saida = m;
+                temp = saida -1;
+
+                for(int i=0; i<range; i++){
+                    saida = saida + temp;
+                    temp = saida -1;
+                }  
+            }
+
+            if(m<n){
+                range = m - n;
+                saida = m;
+                temp = saida +1;
+
+                for(int i=0; i<range; i++){
+                    saida = saida + temp;
+                    temp = saida +1;
+                }
+            } 
+            cout << saida << endl;    
+    
+        }
+
+        if(n==0){
+            saida = m;
+        }
+        //cout << saida << endl;
+        
+    }
+        
+    
 
     return 0;
 }
