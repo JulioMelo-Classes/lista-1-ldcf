b04ee0b2a68ef7fa49e4a7c5eb6ec29fa039841b
ldcf <luluc.fontes@gmail.com>
2021-06-28 02:36:52 +0000

    ultima tentativa porem os casos nao batem

--- soma_vizinhos/src/main.cpp
+++ soma_vizinhos/src/main.cpp
@@ -14,73 +14,48 @@ using std::endl;
 int main(void)
 {
     vector<int> v;
-    int m=0,n=0,range=0,saida=0,temp=0;
+    int m=0,n=0,range=0,saida=0,temp=0,cont=0;
 
     while(true){
-        
         cin >> std::ws >> m;
         cin >> std::ws >> n;
+
 
         if (cin.eof()){
             break;
         }
         
         if(n>0){
-            if(m>n){
-                range = m - n;
-                saida = m;
-                temp = saida -1;
-
-                for(int i=0; i<range; i++){
-                    saida = saida + temp;
-                    temp = saida -1;
-                }  
-            }
-        
-            if(m<n){
-                range = m - n;
-                saida = m;
-                temp = saida +1;
-
-                for(int i=0; i<range; i++){
-                    saida = saida + temp;
-                    temp = saida +1;
-                }  
-            }
-            cout << saida << endl;    
-        }
-
-        if(n<0){
-            if(m>n){
-                range = m + n;
-                saida = m;
-                temp = saida -1;
-
-                for(int i=0; i<range; i++){
-                    saida = saida + temp;
-                    temp = saida -1;
-                }  
-            }
-
-            if(m<n){
-                range = m - n;
-                saida = m;
-                temp = saida +1;
-
-                for(int i=0; i<range; i++){
-                    saida = saida + temp;
-                    temp = saida +1;
-                }
-            } 
-            cout << saida << endl;    
     
+            saida = m;
+            temp = saida +1;
+
+            for(int i=1; i<n; i++){
+                saida = saida + temp;
+                temp = saida +1;
+            }
+
+            cout << saida << endl;                
         }
 
-        if(n==0){
+        else if (n<0){
+            
             saida = m;
+            temp = saida -1;
+
+            for(int i=0; i<n; i++){
+                saida = saida + temp;
+                temp = saida -1;
+            }
+            cout << saida << endl;
         }
-        //cout << saida << endl;
-        
+
+        else{
+            saida = m;
+            cout << saida << endl;
+        }
+
+        m=0,n=0,saida=0,temp=0,cont=0;
     }
         
     
